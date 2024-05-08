f7aa7fc6431743dac2c3dc9b079ff41d672c352e
Onkar Kasture <ok.kasture@gmail.com>
2024-05-08 22:43:19 +0530

    modified #3

--- project/3_top_demanded_skills.sql
+++ project/3_top_demanded_skills.sql
@@ -9,6 +9,7 @@ SELECT skills, COUNT(job_postings_fact.job_id) AS number_of_jobs
 FROM job_postings_fact
 LEFT JOIN skills_job_dim ON job_postings_fact.job_id=skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id=skills_dim.skill_id
+WHERE job_title_short = 'Data Analyst'
 GROUP BY skills
 ORDER BY number_of_jobs DESC
 LIMIT 5
